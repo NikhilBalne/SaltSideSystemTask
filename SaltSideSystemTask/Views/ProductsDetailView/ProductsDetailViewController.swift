3863d56cb496d9b84269545a82bc08d527a3a483
nikhilbalne <nikhil.b@ecoihub.com>
2020-11-22 00:36:55 +0530

    1.Designed Product Table View Screen. 2.Designed Product Detail Screen.

---
+++ SaltSideSystemTask/Views/ProductsDetailView/ProductsDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProductsDetailViewController.swift
+//  SaltSideSystemTask
+//
+//  Created by Nikhil Balne on 21/11/20.
+//
+
+import UIKit
+import Kingfisher
+
+class ProductsDetailViewController: UIViewController {
+
+    var product : Product? = nil
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var imageViewLabel: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpView()
+    }
+    
+    func setUpView(){
+        self.titleLabel.text = product?.title
+        self.descriptionLabel.text = product?.description
+
+        self.imageViewLabel.kf.setImage(with: URL(string: (product?.image)!)!)
+    }
+    
+}
+
+
